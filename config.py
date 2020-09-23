ef1e4baf23f8c79b077bf2ad45f493c88e55a373
Rafael Antunes <rafaelantunesvieira@outlook.com.br>
2020-09-23 18:16:30 -0300

    Correçaõ do erro no config.py

--- config.py
+++ config.py
@@ -1,21 +1,22 @@
 import psycopg2
 
+
 class config:
     def __init__(self, dadosconexao):
         self.dadosconexao = dadosconexao
 
     def setParametros(self):
-        self.dadosconexao = "host='localhost' dbname='Northwind' user='postgres' password='root'"
+        self.dadosconexao = "host='localhost' dbname='northwind' user='postgres' password='bd123'"
         return self
 
     def alteraBD(self, stringSQL, valores):
-        #iniciar conexão vazio
+        # iniciar conexão vazio
         conn = None
         try:
             # abrir a conexão
             conexao = psycopg2.connect(config.setParametros(self).dadosconexao)
 
-            # abrir a sessão - a transação começa aqui 
+            # abrir a sessão - a transação começa aqui
             sessao = conexao.cursor()
 
             # Executar a remoção na memória RAM
@@ -35,7 +36,7 @@ class config:
         return "sucesso"
 
     def consultaBD(self, stringSQL, valores):
-        #iniciar conexão vazio
+        # iniciar conexão vazio
         conn = None
         try:
             # abrir a conexão
@@ -49,7 +50,7 @@ class config:
 
             # Armazenar os resultados:
             registros = sessao.fetchall()
-            colnames = [desc[0] for desc in sessao.description] 
+            colnames = [desc[0] for desc in sessao.description]
 
             # Comitar a remoçao - fechar a transação
             conexao.commit()
@@ -64,7 +65,7 @@ class config:
                 conn.close()
         return (colnames, registros)
 
-    def cadastravendaBD(self, string_SQL_pedido, string_SQL_produto dadospedido, listaprodutos):
+    def cadastravendaBD(self, string_SQL_pedido, string_SQL_produto, dadospedido, listaprodutos):
         # Inciar a inserção do registro
         conn = None
         try:
@@ -79,7 +80,8 @@ class config:
 
             # Executar a inserção dos produtos na memória RAM - TABELA ORDERDETAILS
             for i in listaprodutos:
-                sessao.execute(string_SQL_produto, (i.idPedido, i.idProduto, i.preco, i.quantidade, i.desconto)) 
+                sessao.execute(string_SQL_produto, (i.idPedido,
+                                                    i.idProduto, i.preco, i.quantidade, i.desconto))
 
             # Comitar a remoçao - fechar a transação
             conexao.commit()
@@ -92,4 +94,4 @@ class config:
         finally:
             if conn is not None:
                 conn.close()
-        return "sucesso";
+            return "sucesso"
