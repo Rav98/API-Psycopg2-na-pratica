b593c931d80272e3a25da87ec0e53743331745b7
fmgflavio <fmgflavio@gmail.com>
2020-09-23 17:52:45 -0300

    Arquivos config.py e modelm.py

--- config.py
+++ config.py
@@ -65,3 +65,31 @@ class config:
         return (colnames, registros)
 
     def cadastravendaBD(self, string_SQL_pedido, string_SQL_produto dadospedido, listaprodutos):
+        # Inciar a inserção do registro
+        conn = None
+        try:
+            # abrir a conexão
+            conexao = psycopg2.connect(config.setParametros(self).dadosconexao)
+
+            # abrir a sessão - a transação começa aqui
+            sessao = conexao.cursor()
+
+            # Executar a inserção do pedido na memória RAM - TABELA ORDERS
+            sessao.execute(string_sql_pedido, dadospedido)
+
+            # Executar a inserção dos produtos na memória RAM - TABELA ORDERDETAILS
+            for i in listaprodutos:
+                sessao.execute(string_SQL_produto, (i.idPedido, i.idProduto, i.preco, i.quantidade, i.desconto)) 
+
+            # Comitar a remoçao - fechar a transação
+            conexao.commit()
+
+            # Encerrar a sessão
+            sessao.close()
+
+        except psycopg2.Error:
+            return psycopg2.Error
+        finally:
+            if conn is not None:
+                conn.close()
+        return "sucesso";
---
+++ modelM.py
@@ -0,0 +1,76 @@
+import psycopg2
+from decimal import *
+from config import config
+from psycopg2.extensions import AsIs
+from datetime import datetime
+
+class ProdutoM():
+    def __init__(self, productid, productname, supplierid, categoryid, quantityperunit, unitprice, unitsinstock, unitsonorder, reorderlevel, discontinued):
+        self.id = productid
+        self.nome = productname
+        self.fornecedor = supplierid
+        self.categoria = categoryid
+        self.quantidadeEmbalagem = quantityperunit
+        self.precoUnitario = unitprice
+        self.estoque = unitsinstock
+        self.vendas = unitsonorder
+        self.nivel = reorderlevel
+        self.descontinuado = discontinued
+    
+    def criaProduto(listaValores):
+        produto = ProdutoM(int(listaValores[0]), str(listaValores[1]), int(listaValores[2]), int(listaValores[3]),
+             str(listaValores[4]), Decimal(listaValores[5]),
+             int(listaValores[6]), int(listaValores[7]), int(listaValores[8]), str(listaValores[9]))
+        return produto
+    
+    def cadastraProduto(produto):
+        string_sql = 'INSERT INTO northwind.products (productid, productname, supplierid, categoryid, quantityperunit, unitprice, ' \
+                     'unitsinstock, unitsonorder, reorderlevel, discontinued) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        novo_registro = (produto.id, produto.nome, produto.fornecedor, produto.categoria, produto.quantidadeEmbalagem, produto.precoUnitario, produto.estoque, produto.vendas, produto.nivel, produto.descontinuado)
+        status = config.alteraBD(config, string_sql, novo_registro)
+        return status;
+
+    def deletaproduto(id):
+        string_sql = 'DELETE FROM northwind.products WHERE productid = %s;'
+        status = config.alteraBD(config, string_sql, [id])
+        return status;
+
+    def consultaproduto(id):
+        string_sql = 'SELECT * FROM northwind.products WHERE productid = %s;'
+        registros = config.consultaBD(config, string_sql, [id])
+        if(len(registros[1]) != 0):
+            prod = ProdutoM.criaProduto(registros[1][0])
+            return prod
+        else:
+            return None
+
+    def atualizaproduto(l):
+        string_sql = """UPDATE northwind.products SET %s = %s WHERE productid = %s"""
+        parametros = ((AsIs(l[1])), int(l[2]), int (l[0]))
+        status = config.alteraBD(config, stringsql, parametros)
+        return status
+
+class PedidoM():
+    def consultarelatorio(id):
+        if (id == -1):
+            string_sql = """SELECT * FROM northwind.relatorio"""
+            registros = config.consultaBD(config, string_sql, [[]])
+        else:
+            string_sql = """SELECT * FROM northwind.relatorio WHERE numpedido = %s"""
+            registros = config.consultaBD(config, string_sql, [id])
+        if(len(registros[1]) == 0):
+            registros = None
+        return registros
+    
+    def cadastravendas(dadospedido, listaprodutos):
+        dadospedido =[258952, 2, 2, datetime(2020, 9, 21, 0, 0), datetime(2020, 9, 22, 0, 0), datetime(2020, 9, 23, 0, 0), Decimal('36'), 'abc',]
+        string_SQL_pedido = """INSERT INTO northwind.orders(orderid, customerid, empoyerid, overdate, requiredate, shippeddate, freight, shipname) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
+        string_SQL_produto = """INSERT INTO northwind.order_details(orderid, productid, unitprice, quantity, discount) VALUES (%s, %s, %s, %s, %s);"""
+        status = config.cadastravendaBD(config, string_SQL_pedido, string_SQL_produto, dadospedido, listaprodutos)
+        return status
+
+    def alteravenda(dadospedido):
+        string_sql = """UPDATE northwind.order_details SET quantity = %s WHERE orderid = %s AND productid = %s"""
+        parametros = (dadospedido[2], dadospedido[0], dadospedido[1])
+        status = config.alteraBD(config, string_sql, parametros);
+        return status
